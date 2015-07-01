b9e503c24b89e6f3f0224ed36f60f3f03662cb5e
slauriere <slauriere@gmail.com>
2015-07-01 17:45:18 +0200

    green/yellow/red

--- styler.js
+++ styler.js
@@ -2,12 +2,22 @@ var data = require('./data.geo.json');
 var _ = require('lodash');
 var Fs = require('fs');
 
+var GREEN = '#00931c';
+var YELLOW = '#f9f600';
+var RED = '#ac0000';
+
 _.each(data.features, function(d){
     d.properties.emitterId = _.uniqueId('em-');
     d.properties.railway;
-    d.properties['marker-color'] = '#ac0000';
+    var color = GREEN;
+    var val = Math.floor(Math.random() * 6) + 1;  
+    if (val==4 || val==5)
+    	color = YELLOW;
+    else if (val == 6)
+    	color = RED;
+    d.properties['marker-color'] = color;
     d.properties['marker-size'] = 'small';
-    d.properties ['marker-symbol'] = 'cross';    
+    d.properties ['marker-symbol'] = 'cross';  
 });
 
 
