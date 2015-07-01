6cec968511ecf37b3f51556ed5f680c619c34c51
slauriere <slauriere@gmail.com>
2015-07-01 17:53:19 +0200

    new style

--- styler.js
+++ styler.js
@@ -3,21 +3,21 @@ var _ = require('lodash');
 var Fs = require('fs');
 
 var GREEN = '#00931c';
-var YELLOW = '#f9f600';
+var YELLOW = '#dedc1c';
 var RED = '#ac0000';
 
 _.each(data.features, function(d){
     d.properties.emitterId = _.uniqueId('em-');
     d.properties.railway;
     var color = GREEN;
-    var val = Math.floor(Math.random() * 6) + 1;  
+    var val = Math.floor(Math.random() * 20) + 1;  
     if (val==4 || val==5)
     	color = YELLOW;
     else if (val == 6)
     	color = RED;
     d.properties['marker-color'] = color;
     d.properties['marker-size'] = 'small';
-    d.properties ['marker-symbol'] = 'cross';  
+    d.properties ['marker-symbol'] = 's';  
 });
 
 
